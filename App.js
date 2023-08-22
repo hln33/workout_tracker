31331c786d65d900f2583ab5d0397fecd8f27d92
hln5 <hln5@sfu.ca>
2023-08-22 10:20:06 -0700

    init

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  pressed = () => {
+    alert("hi");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Workout Tracker</Text>
       <StatusBar style="auto" />
+
+      <Button 
+        onPress={pressed}
+        title="Press Me" 
+      />        
     </View>
   );
 }
