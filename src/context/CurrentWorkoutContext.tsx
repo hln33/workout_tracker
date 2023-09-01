e00e0cedfd7e0297734c19d63e1af18394882ecd
hln5 <hln5@sfu.ca>
2023-09-01 11:20:09 -0700

    refactor state into context for Workout.tsx

--- src/context/CurrentWorkoutContext.tsx
+++ src/context/CurrentWorkoutContext.tsx
@@ -1,14 +1,15 @@
 import { createContext, ReactNode, useState } from "react";
 import { Workout } from "../types";
 
+
+const DefaultWorkout: Workout = {name: '', exercises: []}
+
 type CurrentWorkoutContextType = {
-  workout: Workout | null;
+  workout: Workout;
   setWorkout: (workout: Workout) => void;
 };
-
-
 export const CurrentWorkoutContext = createContext<CurrentWorkoutContextType>({
-  workout: null,
+  workout: DefaultWorkout,
   setWorkout: () => null
 });
 
@@ -17,7 +18,7 @@ interface Props {
   children: ReactNode;
 };
 export const CurrentWorkoutProvider = (props: Props) => {
-  const [workout, setWorkout] = useState<Workout | null>(null);
+  const [workout, setWorkout] = useState<Workout>(DefaultWorkout);
   
   return (
     <CurrentWorkoutContext.Provider value={{ workout, setWorkout }}>
--- src/screens/Workout/Workout.tsx
+++ src/screens/Workout/Workout.tsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from 'react';
 import { Button, FlatList, Text, TextInput, View } from 'react-native';
-import { CurrentWorkoutContext, CurrentWorkoutProvider } from '../../context/CurrentWorkoutContext';
+import { CurrentWorkoutContext } from '../../context/CurrentWorkoutContext';
 import { Exercise } from '../../types';
 import { ExerciseInput } from './index';
 import { ExerciseDisplay } from './ExerciseDisplay/index';
@@ -8,21 +8,19 @@ import { ExerciseDisplay } from './ExerciseDisplay/index';
 
 export const Workout = () => {
   const { workout, setWorkout } = useContext(CurrentWorkoutContext);
+  const [notes, setNotes] = useState('');
   console.log(workout);
 
-  const [workoutName, setWorkoutName] = useState('');
-  const [notes, setNotes] = useState('');
-  const [exercises, setExercises] = useState<Exercise[]>(workout?.exercises ?? []);
-
+  const { name, exercises } = workout;
   return (
     <>
-      <TextInput placeholder='Workout Name' onChangeText={e => setWorkoutName(e)} />
+      <TextInput placeholder='Workout Name' onChangeText={newName => setWorkout({ ...workout, name: newName })} />
       <TextInput 
         multiline={true}
         placeholder='Notes'
         onChangeText={e => setNotes(e)}
       />
-      <ExerciseInput onAdd={(exercise: Exercise) => setExercises([...exercises, exercise])}/>
+      <ExerciseInput onAdd={(exercise: Exercise) => setWorkout({ ...workout, exercises: [...exercises, exercise]})} />
       
       <FlatList
         data={exercises}
@@ -30,8 +28,7 @@ export const Workout = () => {
       />
 
       <Button title='Finish' />
-
-      <Button title='context' onPress={() => setWorkout({name: workoutName, exercises: exercises})} />
+      <Button title='Save' onPress={() => setWorkout({name: name, exercises: exercises})} />
     </>
   );
 };
