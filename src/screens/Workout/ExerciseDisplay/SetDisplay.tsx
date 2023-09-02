794223af592bf9dcba5171ae8590081c396ca3de
hln5 <hln5@sfu.ca>
2023-09-02 10:17:04 -0700

    can have reps and weight persist

--- src/screens/Workout/ExerciseDisplay/SetDisplay.tsx
+++ src/screens/Workout/ExerciseDisplay/SetDisplay.tsx
@@ -10,48 +10,51 @@ interface Props {
   set: Set;
 };
 export const SetDisplay = (props: Props) => {
-  const [weight, setWeight] = useState<number>(props.set.lbs);
-  const [reps, setReps] = useState<number>(props.set.reps);
   const { workout, setWorkout } = useContext(CurrentWorkoutContext);
 
+  const id = props.set.id;
   const sets = getCurrentWorkoutSets(workout, props.exerciseName);
+  const set = sets.find(s => s.id === id);
+
+  let weight = 0;
+  let reps = 0;
+  if (set !== undefined) {
+    weight = set.lbs;
+    reps = set.reps;
+  }
+
   const updateWorkoutSets = (updatedSets: Set[]) => {
     let updatedExercises = workout.exercises.map(e => e.name === props.exerciseName ? {...e, sets: updatedSets} : e);
     setWorkout({ ...workout, exercises: updatedExercises });
   };
 
-  const id = props.set.id
   const onDelete = () => {
     let updatedSets = [...sets].filter(set => set.id !== id);
-    updatedSets.forEach((set, newId) => {
-      set.id = newId;
-    });
+    updatedSets.forEach((set, newId) => set.id = newId);
     updateWorkoutSets(updatedSets);
   };
-  const onUpdate = () => {
-    let updatedSets = sets.map(s => (s.id === id) ? {...s, lbs: weight, reps: reps} : s);
+  const onWeightUpdate = (newWeight: number) => {
+    let updatedSets = sets.map(s => (s.id === id) ? {...s, lbs: newWeight} : s);
+    updateWorkoutSets(updatedSets);
+  };
+  const onRepsUpdate = (newReps: number) => {
+    let updatedSets = sets.map(s => (s.id === id) ? {...s, reps: newReps} : s);
     updateWorkoutSets(updatedSets);
   };
 
   return (
     <View style={{flexDirection: 'row'}}>
-      <Text style={{flex: 1}}>{props.set.id + 1}</Text>
+      <Text style={{flex: 1}}>{id + 1}</Text>
 
       <NumericInput 
         style={{flex: 1}}
         placeholder={weight} 
-        onChangeText={e => {
-          setWeight(e);
-          onUpdate();
-        }}
+        onChangeText={e => onWeightUpdate(e)}
       />
       <NumericInput 
         style={{flex: 1}} 
         placeholder={reps} 
-        onChangeText={e => {
-          setReps(e);
-          onUpdate();
-        }}
+        onChangeText={e => onRepsUpdate(e)}
       />
 
       <DeleteButton onDelete={onDelete} />
