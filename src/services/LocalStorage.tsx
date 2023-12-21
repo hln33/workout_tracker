a601e455190ec0575b9636e6bb27bf0bb9006a9b
hln5 <hln5@sfu.ca>
2023-12-20 16:51:30 -0800

    add constant to identify workouts in local storage

--- src/services/LocalStorage.tsx
+++ src/services/LocalStorage.tsx
@@ -1,10 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { WorkoutType } from '@Types';
 
+
+const WORKOUT_IDENTIFIER = 'workout:'
+
+
 export const saveWorkout = async (workout:WorkoutType) => {
   console.log('saving workout to local storage');
-  const key = workout.timestamp.toISOString().substring(0, 10);
+  const key = WORKOUT_IDENTIFIER + workout.timestamp.toISOString().substring(0, 10);
   const value = JSON.stringify(workout);
+  // console.log(key);
+  // console.log(value);
+
   try {
     await AsyncStorage.setItem(key, value);
   } catch (e) {
@@ -14,7 +21,7 @@ export const saveWorkout = async (workout:WorkoutType) => {
 
 export const getWorkout = async (date:Date): Promise<WorkoutType|null> => {
   console.log('getting workout from local storage')
-  const key = date.toISOString().substring(0, 10);
+  const key = WORKOUT_IDENTIFIER + date.toISOString().substring(0, 10);
   try {
     const value = await AsyncStorage.getItem(key); 
     return parseWorkout(value);
