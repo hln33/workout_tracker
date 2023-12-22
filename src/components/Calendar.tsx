3dc7c138b7e4693eef22ef6e18a6d0557d6987c9
hln5 <hln5@sfu.ca>
2023-12-22 11:44:52 -0800

    move app logic from calendar component to screen component

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,38 +1,18 @@
 import { Calendar as CalendarDependency } from 'react-native-calendars';
-import { useEffect, useState } from 'react';
-import { getAllWorkoutDates } from '@Services';
 
 
-interface MarkedDates {
-  [date: string]: { selected: boolean }
-}
 interface Props {
   onDayPress: ((date: Date) => void);
+  markedDates: {};
 }
 export const Calendar = (props: Props) => {
-  const [markedDates, setMarkedDates] = useState<MarkedDates>();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const workoutDates = await getAllWorkoutDates();
-      const selectedDates = workoutDates.reduce((acc, date) => {
-        acc[date] = { selected: true }
-        return acc;
-      }, {} as MarkedDates); 
-
-      setMarkedDates(selectedDates);
-    }
-  
-    fetchData();
-  }, []);
-
   return (
     <CalendarDependency 
       onDayPress={ day => {
-        let date = new Date(day.dateString);
+        const date = new Date(day.dateString);
         props.onDayPress(date);
       }}
-      markedDates={markedDates}
+      markedDates={props.markedDates}
     />
   )
 } 
---
+++ src/screens/History/History.tsx
@@ -0,0 +1,41 @@
+import { Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Calendar } from '@Components';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { AppStackParamList } from '@Navigation';
+import { getAllWorkoutDates } from '@Services';
+
+
+interface MarkedDates {
+  [date: string]: { selected: boolean }
+}
+type Props = NativeStackScreenProps<AppStackParamList, 'History'>
+export const History = ({ navigation }: Props) => {
+  const [markedDates, setMarkedDates] = useState<MarkedDates>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const workoutDates = await getAllWorkoutDates();
+      const selectedDates = workoutDates.reduce((acc, date) => {
+        acc[date] = { selected: true }
+        return acc;
+      }, {} as MarkedDates); 
+
+      setMarkedDates(selectedDates);
+    }
+  
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Text>Calendar</Text>
+      <Calendar 
+        onDayPress={ date => {
+          navigation.navigate('Workout', {dateISOString: date.toISOString()});
+        }}
+        markedDates={markedDates}
+      />
+    </>
+  );
+};
